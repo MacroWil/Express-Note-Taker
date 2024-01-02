9e8ba3cfb6e53e25d0c66d249a2c440239ca56f5
macrowil <macrow.co@gmail.com>
2024-01-02 10:51:00 -0700

    added routing to server.js

--- server.js
+++ server.js
@@ -11,31 +11,6 @@ const { v4: uuidv4 } = require("uuid");
 app.use(express.static("public"));
 app.use(express.json());
 
-//get notes from server
-app.get("/api/notes", (req, res) => {
-  fs.readFile("./db/db.json", (err, data) => {
-    if (err) throw err;
-    let dbData = JSON.parse(data);
-    res.json(dbData);
-  });
-});
-
-//post new notes to server
-app.post("/api/notes", (req, res) => {
-  const newNote = req.body;
-  newNote.id = uuidv4();
-  db.push(newNote);
-  fs.writeFileSync("./db/db.json", JSON.stringify(db));
-  res.json(db);
-});
-
-//delete from server when pressing delete
-app.delete("/api/notes/:id", (req, res) => {
-  const newDb = db.filter((note) => note.id !== req.params.id);
-  fs.writeFileSync("./db/db.json", JSON.stringify(newDb));
-  readFile.json(newDb);
-});
-
 // routing
 app.get("/notes", (req, res) => {
   res.sendFile(path.join(__dirname, "public", "notes.html"));
